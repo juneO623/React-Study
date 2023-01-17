2f3ed3a1436bb151cabfb9202ba77a53a077b0e9
username <81845513+june3011@users.noreply.github.com>
2023-01-17 18:04:43 +0900

    Study : css module

--- Next-Study/nextjs-intro/components/Navbar.js
+++ Next-Study/nextjs-intro/components/Navbar.js
@@ -1,5 +1,6 @@
 import Link from "next/link";
 import { useRouter } from "next/router";
+import styles from "./Navbar.module.css";
 
 export default function Navbar() {
   const router = useRouter();
@@ -8,12 +9,21 @@ export default function Navbar() {
   return (
     <nav>
       <Link href="/">
-        <span style={{ color: router.pathname === "/" ? "red" : "blue" }}>
+        <span
+          className={`${styles.link} ${
+            router.pathname === "/" ? styles.active : ""
+          }`}
+        >
           Home
         </span>
       </Link>
       <Link href="/about">
-        <span style={{ color: router.pathname === "/about" ? "red" : "blue" }}>
+        <span
+          className={[
+            styles.link,
+            router.pathname === "/about" ? styles.active : "",
+          ].join(" ")}
+        >
           About
         </span>
       </Link>
